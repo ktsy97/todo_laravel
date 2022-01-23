13380dcdf083d7e5996a42a4a6d8c6135e56c56b
KONDO Katsuya <k0d0ktsy@gmail.com>
2022-01-23 14:02:38 +0900

    タスク一覧画面の開発

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks');
