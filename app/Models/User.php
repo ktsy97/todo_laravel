fa4dbb292b504be92e2c41f18ca773362d9cdaff
KONDO Katsuya <k0d0ktsy@gmail.com>
2022-01-23 16:16:24 +0900

    ログイン機能の開発

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * ユーザーの保持する全タスク
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
